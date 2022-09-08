043ccd01df82349cd9150531403f2df292b9273e
Gabriel Nascimento <gabrielnpoa@gmail.com>
2022-09-08 18:29:30 -0300

    project fineshed

---
+++ db/index.ts
@@ -0,0 +1,6 @@
+const stringDb =
+  "1234567890-=!@#$%¨&*()_+qwertyuiop[QWERTYUIOP{asdfghjklç]ASDFGHJKLÇ}zxcvbnm,.;|ZXCVBNM<>:/?";
+
+const database = stringDb.split("");
+
+export default database;
